35705d217de8789cfd120e05eabe2b562a346402
Singh <Harjeev.Singh@cerner.com>
2022-01-20 16:07:36 +0530

    Rest Api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { argv } = require("process");
 const router = express.Router();
 let bodyParser = require('body-parser');
 
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/getP',(req,res)=>{
