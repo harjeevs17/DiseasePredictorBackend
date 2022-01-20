b49f6a424e31b638152216826bebee14fa577a2f
Singh <Harjeev.Singh@cerner.com>
2022-01-20 15:22:16 +0530

    Rest Api

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const http = require("http").Server(app);
+var cors = require("cors");
+const { argv } = require("process");
+const router = express.Router();
+let bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/getP',(req,res)=>{
+    let data = req.body.symptoms.toString().split(",")
+    var payload = ["./diseasePrediction.py"]
+    data.map((item,key)=>{
+        payload.push(item)
+    })
+    const { spawn } = require('child_process');
+    const pyProg = spawn('python',payload);
+    let respose = ''
+    pyProg.stdout.on('data', function(data) {
+        res.end(data.toString().trim())
+
+    });
+})
+const server = http.listen(PORT, () => {
+    console.log("Listening to port", PORT);
+});
