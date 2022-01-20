53cedd00dfc9f7b99403fb0690adca737dc6acef
Singh <Harjeev.Singh@cerner.com>
2022-01-20 22:58:18 +0530

    final changes

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ const { argv } = require("process");
 const router = express.Router();
 let bodyParser = require('body-parser');
 
-//app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/getP',(req,res)=>{
     let data = req.body.symptoms.toString().split(",")
-    var payload = ["./hello.py"]
+    var payload = ["./diseasePrediction.py"]
     data.map((item,key)=>{
         payload.push(item)
     })
-    console.log("mappedd");
     const { spawn } = require('child_process');
     const pyProg = spawn('python',payload);
     let respose = ''
     pyProg.stdout.on('data', function(data) {
         res.end(data.toString().trim())
+
     });
-    console.log("weqeqw");
 })
 const server = http.listen(PORT, () => {
     console.log("Listening to port", PORT);
