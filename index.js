42a48e173d5e2facb73fe0f3859ff8186d62f929
Singh <Harjeev.Singh@cerner.com>
2022-02-07 23:35:08 +0530

    test

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const http = require("http").Server(app);
-var cors = require("cors");
 const { argv } = require("process");
 const router = express.Router();
 let bodyParser = require('body-parser');
 var timeout = require('connect-timeout')
-
+var cors = require("cors");
 app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
