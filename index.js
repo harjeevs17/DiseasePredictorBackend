34510a6199aef30dd1b6d2996c802788a1292bcc
Singh <Harjeev.Singh@cerner.com>
2022-02-07 22:38:19 +0530

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const http = require("http").Server(app);
-var cors = require("cors");
+
 const { argv } = require("process");
 const router = express.Router();
 let bodyParser = require('body-parser');
