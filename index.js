559d97e824026a7beadc5cb97dc20c273c340ed6
Singh <Harjeev.Singh@cerner.com>
2022-02-07 18:34:21 +0530

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.post('/getP',(req,res)=>{
+/*app.post('/getP',(req,res)=>{
     let data = req.body.symptoms.toString().split(",")
     var payload = ["./diseasePrediction.py"]
     data.map((item,key)=>{
@@ -23,6 +23,10 @@ app.post('/getP',(req,res)=>{
         res.end(data.toString().trim())
 
     });
+})*/
+
+app.get('/hello',(req,res)=>{
+   res.end("Hello")
 })
 const server = http.listen(PORT, () => {
     console.log("Listening to port", PORT);
